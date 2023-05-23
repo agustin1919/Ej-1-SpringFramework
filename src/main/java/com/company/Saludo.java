02f65aa4a3fec11dbd2d873e89ed90b02e749fc7
FacundoS <agustinsouyaux@gmail.com>
2023-05-23 12:33:33 -0300

    Primer Commit

---
+++ src/main/java/com/company/Saludo.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Saludo {
+
+    public Saludo(){
+        System.out.println("Pasando por constructor de saludo");
+    }
+    public void imprimirSaludo(){
+        System.out.println("Saludo");
+    }
+}
